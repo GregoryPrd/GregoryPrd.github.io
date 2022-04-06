3b767e9bdab3c392e2287e541d1fd12f3e9eeeef
GregoryPrd <102628945+GregoryPrd@users.noreply.github.com>
2022-04-06 21:25:27 +0200

    Add files via upload

--- app2.js
+++ app2.js
@@ -1,14 +1,14 @@
 function themedark() {
     document.getElementById('light').style.display = 'block';
      document.getElementById('dark').style.display = 'none';
-     document.body.style.color = "#A9CBD7";
-     document.body.style.backgroundColor = "#03224C";
+     document.body.style.color = "#ffffff";
+     document.body.style.backgroundColor = "#222222";
 }
 
 
 function themeclair(){
     document.getElementById('dark').style.display = 'block';
     document.getElementById('light').style.display = 'none';
-    document.body.style.color = "#03224C";
-    document.body.style.backgroundColor = "#A9CBD7";
+    document.body.style.color = "#222222";
+    document.body.style.backgroundColor = "#ffffff";
 }
