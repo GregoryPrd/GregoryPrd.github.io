b22a3daeee1d5b9c8c4039225aeb62423f862c11
GregoryPrd <102628945+GregoryPrd@users.noreply.github.com>
2022-06-24 01:42:02 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,35 +1,8 @@
-let img__slider = document.getElementsByClassName('img__slider');
-
-let etape = 0;
-
-let nbr__img = img__slider.length;
-
-let precedent = document.querySelector('.precedent');
-let suivant = document.querySelector('.suivant');
-
 function enleverActiveImages() {
-    for(let i = 0 ; i < nbr__img ; i++) {
+    for(i = 0 ; i < nbr__img ; i++) {
         img__slider[i].classList.remove('active');
     }
 }
-
-suivant.addEventListener('click', function() {
-    etape++;
-    if(etape >= nbr__img) {
-        etape = 0;
-    }
-    enleverActiveImages();
-    img__slider[etape].classList.add('active');
-})
-
-precedent.addEventListener('click', function() {
-    etape--;
-    if(etape < 0) {
-        etape = nbr__img - 1;
-    }
-    enleverActiveImages();
-    img__slider[etape].classList.add('active');
-})
 
 setInterval(function() {
     etape++;
@@ -38,15 +11,15 @@ setInterval(function() {
     }
     enleverActiveImages();
     img__slider[etape].classList.add('active');
-}, 5000)
-
+}, 5000);
 
 
 function themedark() {
     document.getElementById('light').style.display = 'block';
-     document.getElementById('dark').style.display = 'none';
-     document.body.style.color = "#ffffff";
-     document.body.style.backgroundColor = "#222222";
+    document.getElementById('dark').style.display = 'none';
+    document.body.style.color = "#ffffff";
+    document.body.style.backgroundColor = "#222222";
+    setCookie('theme', 1);
 }
 
 
@@ -55,4 +28,59 @@ function themeclair(){
     document.getElementById('light').style.display = 'none';
     document.body.style.color = "#222222";
     document.body.style.backgroundColor = "#ffffff";
+    setCookie('theme', 2);
+}
+
+function setCookie(key, value) {
+    document.cookie = key + '=' + value;
+}
+
+function getCookie(key) {
+    var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+    return keyValue ? keyValue[2] : null;
+}
+
+/** COOKIES THEME **/
+
+var theme = getCookie('theme');
+
+if (theme !== null) {
+    if(theme === '1')
+        themedark();
+    else
+        themeclair();
+}
+
+
+
+/** GESTION SLIDER **/
+
+img__slider = document.getElementsByClassName('img__slider');
+
+etape = 0;
+
+nbr__img = img__slider.length;
+
+precedent = document.querySelector('.precedent');
+suivant = document.querySelector('.suivant');
+
+if (suivant !== null) {
+
+    suivant.addEventListener('click', function() {
+        etape++;
+        if(etape >= nbr__img) {
+            etape = 0;
+        }
+        enleverActiveImages();
+        img__slider[etape].classList.add('active');
+    });
+
+    precedent.addEventListener('click', function() {
+        etape--;
+        if(etape < 0) {
+            etape = nbr__img - 1;
+        }
+        enleverActiveImages();
+        img__slider[etape].classList.add('active');
+    });
 }
